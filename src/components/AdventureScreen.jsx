5f5cedc2d99d9f95165ecb93e1f7544a35f2a2b9
RynRyn <ryn.houle@gmail.com>
2025-11-20 16:33:57 -0500

    adds possible locations and placeholder images for adventures

--- src/components/AdventureScreen.jsx
+++ src/components/AdventureScreen.jsx
@@ -1,33 +1,49 @@
 import React from 'react'
 import useGameStore from '../hooks/useGameStore'
+import { adventureLocations } from '../data/adventures'
 
 const AdventureScreen = () => {
-    const { setScreen, startAdventure, currentAdventure } = useGameStore();
-    {currentAdventure && (
-        <p>Returning in {Math.ceil((currentAdventure.endTime - Date.now()) / 1000)} seconds...</p>
-    )}
+    const { currentAdventure, setScreen, startAdventure } = useGameStore();
+
+    const adventureData = currentAdventure
+        ? adventureLocations[currentAdventure.location]
+        : null;
 
     return (
         <div>
-            <h2>Adventure!</h2>
+        <h2>Adventure!</h2>
 
-            {currentAdventure ? (
-                <div>
-                    <p>Your pet is on an adventure...</p>
-                    <p>Returning in{" "} {Math.max(0, Math.ceil((currentAdventure.endTime - Date.now()) / 1000))} seconds...</p>
-                </div>
-            )
-            :
-            ( 
-            <button onClick={startAdventure}>Start Adventure</button>
-            )}
-
-            <button onClick={() => setScreen("home")}>
-            Back Home
+        {currentAdventure ? (
+            <div>
+            <h3>{adventureData?.name}</h3>
+            <img
+                src={adventureData?.image}
+                alt={adventureData?.name}
+                width={200}
+            />
+            <p>{adventureData?.description}</p>
+            <p>
+                Returning in{' '}
+                {Math.max(
+                0,
+                Math.ceil((currentAdventure.endTime - Date.now()) / 1000)
+                )}{' '}
+                seconds...
+            </p>
+            </div>
+        ) : (
+            <div>
+            <p>No adventure in progress.</p>
+            <button onClick={() => startAdventure('Forest')}>
+                Start Forest Adventure
             </button>
+            </div>
+        )}
+
+        <button onClick={() => setScreen('home')}>Back Home</button>
         </div>
     );
-}
+};
 
 
 export default AdventureScreen
---
+++ src/data/adventures.js
@@ -0,0 +1,69 @@
+import forestImg from '../assets/Forest.png';
+import caveImg from '../assets/Cave.png';
+import beachImg from '../assets/Beach.png';
+import cityImg from '../assets/City.png';
+import desertImg from '../assets/Desert.png';
+import labImg from '../assets/Lab.png';
+import parlorImg from '../assets/Parlor.png';
+import seafloorImg from '../assets/Seafloor.png';
+import snowImg from '../assets/Snow.png';
+import spaceImg from '../assets/Space.png';
+import volcanoImg from '../assets/Volcano.png';
+
+export const adventureLocations = {
+    Forest: {
+        name: 'Forest',
+        image: forestImg,
+        description: 'Placeholder description',
+    },
+    Cave: {
+        name: 'Cave',
+        image: caveImg,
+        description: 'Placeholder description',
+    },
+    Beach: {
+        name: 'Beach',
+        image: beachImg,
+        description: 'Placeholder description',
+    },
+    City: {
+        name: 'City',
+        image: cityImg,
+        description: 'Placeholder description',
+    },
+    Desert: {
+        name: 'Desert',
+        image: desertImg,
+        description: 'Placeholder description',
+    },
+    Lab: {
+        name: 'Lab',
+        image: labImg,
+        description: 'Placeholder description',
+    },
+    Parlor: {
+        name: 'Parlor',
+        image: parlorImg,
+        description: 'Placeholder description',
+    },
+    Seafloor: {
+        name: 'Seafloor',
+        image: seafloorImg,
+        description: 'Placeholder description',
+    },
+    Snow: {
+        name: 'Snow',
+        image: snowImg,
+        description: 'Placeholder description',
+    },
+    Space: {
+        name: 'Space',
+        image: spaceImg,
+        description: 'Placeholder description',
+    },
+    Volcano: {
+        name: 'Volcano',
+        image: volcanoImg,
+        description: 'Placeholder description',
+    },
+};

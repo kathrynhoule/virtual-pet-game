8d94025681cc3424a282ac0fca1ab0b2bc426532
RynRyn <ryn.houle@gmail.com>
2025-11-21 16:14:06 -0500

    moved a bunch of stuff out of the main return so it's easier to read, also made some individual parts easier to read for later as well

--- src/components/AdventureScreen.jsx
+++ src/components/AdventureScreen.jsx
@@ -8,67 +8,90 @@ import { adventureLocations } from '../data/adventures'
 const AdventureScreen = () => {
     const { currentAdventure, setScreen, startAdventure, pet } = useGameStore();
 
+    //locations the pet has unlocked based on adventures completed
     const unlockedLocations = Object.values(adventureLocations).filter(
         loc => pet.adventuresCompleted >= (loc.unlockAt ?? 0)
     );
 
-    const adventureData = currentAdventure
-        ? adventureLocations[currentAdventure.location]
-        : null;
+    //data for current adventure, if there are any
+    let adventureData = null;
+
+    if (currentAdventure) {
+        adventureData = adventureLocations[currentAdventure.location];
+    }
 
     //pet can't be too hungry or too tired or both to go on an adventure
     const canAdventure = pet.hunger > 0 && pet.energy > 0;
 
+    //calc for remaining seconds of adventure
+    const getRemainingSeconds = () =>
+        Math.max(
+            0,
+            Math.ceil((currentAdventure.endTime - Date.now()) / 1000)
+        );
+
+    //UI
+    const renderAdventureImage = (name, image) =>
+        image ? <img src={image} width={200} alt={name} /> : null;
+
+    const renderActiveAdventure = () => {
+        if (!adventureData) return null;
+
+        const { name, image, description } = adventureData;
+
+        return (
+            <div>
+                <h3>{name}</h3>
+                {renderAdventureImage(name, image)}
+                <p>{description}</p>
+                <p>Returning in {getRemainingSeconds()} seconds...</p>
+            </div>
+        );
+    };
+
+    const renderAdventureSelection = () => (
+        <div>
+            <p>Select an adventure:</p>
+
+            {!canAdventure && (
+                <p style={{ color: "red" }}>
+                    Your pet is too tired or too hungry to go adventuring!
+                </p>
+            )}
+
+            {/* unlocked */}
+            {unlockedLocations.map((loc) => (
+                <button
+                    key={loc.name}
+                    onClick={() => startAdventure(loc.name)}
+                    disabled={!canAdventure}
+                >
+                    Go to {loc.name}
+                </button>
+            ))}
+
+            {/* locked */}
+            {Object.values(adventureLocations)
+                .filter((loc) => !unlockedLocations.includes(loc))
+                .map((loc) => (
+                    <button key={loc.name} disabled>
+                        {loc.name} (Locked)
+                    </button>
+                ))}
+        </div>
+    );
+
+
+    //MAIN RETURN
     return (
         <div>
             <h2>Adventure!</h2>
 
-            {currentAdventure ? (
-                <div>
-                    <h3>{adventureData?.name}</h3>
-                    <img src={adventureData?.image} width={200} />
-                    <p>{adventureData?.description}</p>
-                    <p>
-                        Returning in{" "}
-                        {Math.max(
-                            0,
-                            Math.ceil((currentAdventure.endTime - Date.now()) / 1000)
-                        )}{" "}
-                        seconds...
-                    </p>
-                </div>
-            ) : (
-                <div>
-                    <p>Select an adventure:</p>
+            {currentAdventure
+                ? renderActiveAdventure()
+                : renderAdventureSelection()}
 
-                    {!canAdventure && (
-                        <p style={{ color: "red" }}>
-                            Your pet is too tired or too hungry to go adventuring!
-                        </p>
-                    )}
-
-
-                    {unlockedLocations.map(loc => (
-                        <button
-                            key={loc.name}
-                            onClick={() => startAdventure(loc.name)}
-                            disabled={!canAdventure}
-                        >
-                            Go to {loc.name}
-                        </button>
-                    ))}
-
-                    {Object.values(adventureLocations)
-                        .filter(loc => !unlockedLocations.includes(loc))
-                        .map(loc => (
-                            <button key={loc.name} disabled>
-                                {loc.name} (Locked)
-                            </button>
-                        ))}
-                </div>
-            )}
-
-            <button onClick={() => setScreen('home')}>Back Home</button>
+            <button onClick={() => setScreen("home")}>Back Home</button>
         </div>
     );
 };

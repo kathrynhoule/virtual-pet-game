6ea80e4276407832dec0fa55bc5e73584da14c0b
RynRyn <ryn.houle@gmail.com>
2025-11-23 23:10:00 -0500

    adds data files for future event locations and NPCs with placeholder dialogue and descriptions

---
+++ src/data/eventLocations.js
@@ -0,0 +1,8 @@
+
+export const eventLocations = {
+    EvolutionLab: {
+        name: 'Evolution Lab',
+        description: 'A high-tech laboratory where pets can evolve to new stages.',
+        image: null,
+    },
+}
---
+++ src/data/npcs.js
@@ -0,0 +1,29 @@
+
+export const NPCs = {
+    Shopkeeper: {
+        name: 'Shopkeeper',
+        dialogue: [
+            "Welcome to my shop! Feel free to browse around.",
+            "Looking for something special? I might have just what you need.",
+            "Remember, taking good care of your pet is important!",
+        ],
+        image: null,
+    },
+    RivalOne: {
+        name: 'Rival One',
+        dialogue: [
+            "I bet my pet can beat yours in a competition!",
+            "Have you tried exploring the adventure locations? You might find rare items there.",
+        ],
+        image: null,
+    },
+    RivalTwo: {
+        name: 'Rival Two',
+        dialogue: [
+            "Your pet looks interesting. What's its name?",
+            "Hey! I see you've got a pet too. Let's see whose pet is better!",
+            "I heard there's a new item in the shop that can boost your pet's stats.",
+        ],
+        image: null,
+    },
+};

a033295be627638da25dae1445edfceedfb3b0ea
RynRyn <ryn.houle@gmail.com>
2026-01-09 13:46:30 -0500

    adds messages for when a stat increases from an adventure

---
+++ src/data/statMessages.js
@@ -0,0 +1,34 @@
+//array incase i want to add multiple message options per stat later
+
+export const statMessages = {
+    weird: [
+        "Your pet feels... different somehow.",
+    ],
+    eerie: [
+        "Your pet became more unsettling to look at for very long.",
+    ],
+    style: [
+        "Your pet's fashion sense has improved.",
+    ],
+    grime: [
+        "Your pet came back dirtier than before.",
+    ],
+    neon: [
+        "Your pet is glowing in an unpleasant or pleasant way, depending on how you look at it.",
+    ],
+    wet: [
+        "Your pet is soaked.",
+    ],
+    hot: [
+        "Your pet is panting heavily.",
+    ],
+    mossy: [
+        "Your pet has green things growing on them.",
+    ],
+    cold: [
+        "Your pet is freezing to the touch.",
+    ],
+    mundane: [
+        "Your pet somehow seems more ordinary than before.",
+    ],
+};
---
+++ src/utils/adventureStatMessages.js
@@ -0,0 +1,21 @@
+import { statMessages } from "../data/statMessages";
+
+//generates messages for stat gains after an adventure
+//currently set up to choose from a list of messages for each stat
+//but i currently only have one message per stat
+
+export function generateStatMessages(statGains = {}) {
+    const messages = [];
+
+    for (const stat of Object.keys(statGains)) {
+    const options = statMessages[stat];
+    if (!options || options.length === 0) continue;
+
+    const message =
+        options[Math.floor(Math.random() * options.length)];
+
+    messages.push(message);
+    }
+
+    return messages;
+}
